3e323fcc20ea31c920fb19344a912011e6ffce1b
AnilP. <anl.pirincoglu@gmail.com>
2024-01-12 03:13:06 +0300

    library installation and redux connection

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+import logger from "redux-logger";
+import { legacy_createStore as createStore } from "redux";
+import rootReducer from "./reducers";
 
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <>
+    <Provider store={store}>
       <App />
-    </>
+    </Provider>
   </BrowserRouter>
 );
